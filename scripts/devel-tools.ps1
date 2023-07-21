3b5994882ec11b3d5f3d01f3cc33b1041bed730b
kindtek <98300737+kindtek@users.noreply.github.com>
2023-07-21 07:27:36 -0700

    most everything works

--- scripts/devel-tools.ps1
+++ scripts/devel-tools.ps1
@@ -377,8 +377,8 @@ function start_docker_desktop {
 }
 
 function require_docker_online_new_win {
-    # start_dvlp_process_popmin "require_docker_online" 
-    require_docker_online
+    start_dvlp_process_popmin "require_docker_online" '' '' 
+    # require_docker_online
 }
 
 function require_docker_online {
