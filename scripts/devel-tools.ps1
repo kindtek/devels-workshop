a5fd6d2a8e32b46b5bd12a45fefdcc1b2e4c0b29
kindtek <98300737+kindtek@users.noreply.github.com>
2023-07-21 17:13:59 -0700

    longer sleep

--- scripts/devel-tools.ps1
+++ scripts/devel-tools.ps1
@@ -386,7 +386,7 @@ function require_docker_online {
     $docker_tries = 0
     $docker_cycles = 0
     $docker_settings_reset = $true
-    $sleep_time = 3
+    $sleep_time = 5
     Write-Host "`r`n`r`nloading docker desktop ..."
     Write-Host "waiting for docker backend to come online ..."  
     do {   
@@ -398,10 +398,10 @@ function require_docker_online {
             $docker_tries++
             Write-Host "${docker_cycles}.${docker_tries}"
             if ( (is_docker_desktop_online) -eq $false ) {
-                if ( ($docker_tries -eq 1 -And $docker_cycles -eq 0) -And ((is_docker_backend_online) -eq $false)) {
+                if ( ($docker_tries -eq 1 -And $docker_cycles -eq 0) -And (is_docker_backend_online -eq $false)) {
                     Write-Host "error messages are expected when first starting docker. please wait ..."
                     # give extra time first time through
-                    Start-Sleep -s 10
+                    Start-Sleep -s 15
                 }
                 if (($docker_tries % 2) -eq 0) {
                     write-host ""
