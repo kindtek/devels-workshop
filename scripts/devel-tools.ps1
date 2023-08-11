acbaab171194c02ea7c847c14c9e804563c47143
kindtek <98300737+kindtek@users.noreply.github.com>
2023-08-10 17:57:40 -0700

    dbg trace

--- scripts/devel-tools.ps1
+++ scripts/devel-tools.ps1
@@ -513,7 +513,7 @@ function require_docker_online_new_win {
 }
 
 function require_docker_online {
-    # Set-PSDebug -Trace 2
+    Set-PSDebug -Trace 2
 
     [int]$docker_tries = 0
     [int]$docker_cycles = 0
@@ -617,7 +617,7 @@ function require_docker_online {
     } else {
         Write-Host "could not connect to docker"
     }
-    # Set-PSDebug -Trace 0;
+    Set-PSDebug -Trace 2;
     return $(is_docker_desktop_online)
 }
 
