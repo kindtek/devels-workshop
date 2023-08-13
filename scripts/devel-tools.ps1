7860c7e1b53f6fba378fc8a9a2b6bcf0a2400d96
kindtek <98300737+kindtek@users.noreply.github.com>
2023-08-13 00:54:13 -0700

    suppress ugly errors when testing

--- scripts/devel-tools.ps1
+++ scripts/devel-tools.ps1
@@ -572,13 +572,19 @@ function is_docker_backend_online {
 }
 function is_docker_desktop_online {
     try {
-        $docker_daemon_online = docker search scratch --limit 1 --format helloworld 
-        if (($docker_daemon_online -eq 'helloworld') -And ($(is_docker_backend_online) -eq $true)) {
-            return $true
-        }
-        else {
+        docker search scratch --limit 1 --format helloworld | Out-Null
+        if ($?){
+            $docker_daemon_online = docker search scratch --limit 1 --format helloworld 
+            if (($docker_daemon_online -eq 'helloworld') -And ($(is_docker_backend_online) -eq $true)) {
+                return $true
+            }
+            else {
+                return $false
+            }
+        } else {
             return $false
         }
+        
     }
     catch {
         return $false
