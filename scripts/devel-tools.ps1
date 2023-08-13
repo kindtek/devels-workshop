8a57ff71d8abf88eaa0a1458de3db1cb2a0af5d7
kindtek <98300737+kindtek@users.noreply.github.com>
2023-08-13 02:26:28 -0700

    unset envs during uninstall

--- scripts/devel-tools.ps1
+++ scripts/devel-tools.ps1
@@ -786,6 +786,7 @@ function remove_installation {
     Remove-Item "$env:USERPROFILE/repos/$($env:KINDTEK_WIN_GIT_OWNER)/.github-installed" -Confirm:$false -Force -ErrorAction SilentlyContinue
     Remove-Item "$env:USERPROFILE/kache" -Recurse -Confirm -Force -ErrorAction SilentlyContinue
     Remove-Item "$env:USERPROFILE/.wslconfig" -Confirm -ErrorAction SilentlyContinue
+    unset_dvlp_envs
     # }
 
 }
