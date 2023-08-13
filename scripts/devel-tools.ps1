3b890aab36c11b88fdcb1d7c34e0801d41e64876
kindtek <98300737+kindtek@users.noreply.github.com>
2023-08-13 02:53:15 -0700

    use start-process

--- scripts/devel-tools.ps1
+++ scripts/devel-tools.ps1
@@ -125,8 +125,7 @@ function install_windows_features {
         $skipreboot = 'skip'
     }
     if ((!(Test-Path -Path "$env:KINDTEK_WIN_GIT_PATH/.windows-installed" -PathType Leaf)) -Or $install_anyways -eq 'true') {
-        $winconfig = "$env:KINDTEK_WIN_DVLADV_PATH/add-windows-features.ps1"
-        &$winconfig = Invoke-Expression -command "$env:KINDTEK_WIN_DVLADV_PATH/add-windows-features.ps1 $skipreboot" 
+        Start-Process -ArgumentList "-command '$env:KINDTEK_WIN_DVLADV_PATH/add-windows-features.ps1 $skipreboot'"
     }
     return
 }
