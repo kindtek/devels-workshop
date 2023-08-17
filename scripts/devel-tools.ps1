e7c47d82529dad4aac939b1283d7b2126c31705e
kindtek <98300737+kindtek@users.noreply.github.com>
2023-08-16 23:24:31 -0700

    single distro display fix

--- scripts/devel-tools.ps1
+++ scripts/devel-tools.ps1
@@ -841,18 +841,10 @@ function get_wsl_distro_list {
     $env:WSL_UTF8 = 1
     wsl.exe --list | Out-Null
     if (($?)){
-        $distro_array = (wsl.exe --list | Out-String).split("`r`n").trim() | Where-Object { $_ -And $_ -ne 'Windows Subsystem for Linux Distributions:' }
+        $distro_array = (wsl.exe --list | Out-String).split("`r`n").trim() | Where-Object { (!([string]::isNullOrEmpty($_))) -And ($_ -ne 'Windows Subsystem for Linux Distributions:') -and ($_ -ne "docker-desktop") -and ($_ -ne "docker-desktop-data") -and ($_ -ne "$env:KINDTEK_FAILSAFE_WSL_DISTRO")}
         $distro_array = $distro_array -replace '^(.*)\s.*$', '$1'
         $distro_array = $distro_array -replace "[^a-zA-Z0-9_-]", ''
-        $distro_array_final = @()
-        if ($distro_array.length -gt 1) {    
-            for ($i = 0; $i -le $distro_array.length - 1; $i++) {
-                if ($($distro_array[$i]) -ne "docker-desktop" -And $($distro_array[$i]) -ine "docker-desktop-data" -And $($distro_array[$i]) -ine "$env:KINDTEK_FAILSAFE_WSL_DISTRO" -and !([string]::IsNullOrWhiteSpace($($distro_array[$i]))) -and !([string]::IsNullOrEmpty($($distro_array[$i]))) -and $($distro_array[$i]) -ine '' ) {
-                        $distro_array_final += "$($distro_array[$i])".trim()
-                }
-            }
-        }
-        return $distro_array_final
+        return $distro_array
     } else {
         return @()
     }
