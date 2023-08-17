b6c5dcf0f620ce7f7c26af181e69b47193b506d6
kindtek <98300737+kindtek@users.noreply.github.com>
2023-08-16 22:34:19 -0700

    try to fix character issues

--- scripts/devel-tools.ps1
+++ scripts/devel-tools.ps1
@@ -848,8 +848,7 @@ function get_wsl_distro_list {
             for ($i = 0; $i -le $distro_array.length - 1; $i++) {
                 if ($($distro_array[$i]) -ne "docker-desktop" -And $($distro_array[$i]) -ine "docker-desktop-data" -And $($distro_array[$i]) -ine "$env:KINDTEK_FAILSAFE_WSL_DISTRO" -and !([string]::IsNullOrWhiteSpace($($distro_array[$i]))) -and !([string]::IsNullOrEmpty($($distro_array[$i]))) -and $($distro_array[$i]) -ine '' ) {
                     $replacements = "[^a-zA-Z0-9_-]" 
-                    $distro_array_final = $distro_array[$i] -replace $replacements, ''
-                    $distro_array_final += "$($distro_array[$i])"
+                    $distro_array_final += "$($distro_array[$i] -replace $replacements, '')".trim()
                 }
             } 
         }
